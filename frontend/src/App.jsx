c4470de4c1bb4d225e889bf39676021ea21e3b37
Shashank2958 <shashanktiwari.stg@gmail.com>
2026-04-03 01:07:23 +0530

    file structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+function App() {
+  const [students, setStudents] = useState([])
+  useEffect(() => {
+    async function getAllStudent() {
+      try {
+        const students = await axios.get('http://127.0.0.1:8000/api/student/')
+        console.log(students.data)
+        setStudents(students.data)
+      } catch (error) {
+        console.log('error')
+      }
+
+    }
+    getAllStudent()
+  }, [])
+
+
+  return (
+    <>
+      <div className="App">
+        <h1>connect django to react</h1>
+        {
+          students.map((student, i) => {
+            return (
+              <h2 key={i}> name: {student.stuname} and email: {student.email} </h2>
+            )
+          })
+        }
+      </div>
+    </>
+  );
+}
+
+export default App
